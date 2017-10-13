9580154f3a48ebb85884905ce0d99893c8bdf2ce
Alex Danoff <aadanoff@gmail.com>
2017-10-12 23:45:05 -0400

    Basic functionality
    You can type something into the text area, press enter, and it'll show
    up in big letters!

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $form = $("#mess-form");
+  $form.submit(function (event) {
+    event.preventDefault();
+    $message = $("#message");
+    const raw = $message.val();
+    const escaped = $("<div />").text(raw).html();
+    const to_add = '<h1 class="main-words">' + escaped + '</h1>';
+
+    $message.val('');
+    $('#words-container').html(to_add);
+    $('body').addClass('showing');
+    $('#form-container').hide();
+  });
+});
