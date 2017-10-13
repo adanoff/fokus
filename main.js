cf02c3fb30b72694a2db9935672a2106861c08e9
Alex Danoff <aadanoff@gmail.com>
2017-10-13 01:44:19 -0400

    Add escape key recognition
    When the user presses the escape key, the site resets and allows the
    user to input a new focus.

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
+function reset() {
+  $('#words-container').hide();
+  $('#form-container').show();
+  $('#message').focus();
+  $('body').removeClass('anim-body');
+}
+
 $(document).ready(function() {
-  $form = $("#mess-form");
+  $('#message').focus();
+  $form = $('#mess-form');
   $form.submit(function(event) {
     event.preventDefault();
-    $message = $("#message");
+    $message = $('#message');
     const raw = $message.val();
-    const escaped = $("<div />").text(raw).html();
+    const escaped = $('<div />').text(raw).html();
 
     const $plain = $('<h1 class="plain-words">' + escaped + '</h1>');
     $message.val('');
     $('#form-container').hide();
     $('#words-container').html($plain);
+    $('#words-container').show();
     $plain.removeClass('plain-words');
     $plain.addClass('anim-words');
     $('body').addClass('anim-body');
   });
+
+  $(document).keyup(function(event) {
+    // 27 == ESC
+    if (event.which == 27) {
+      reset();
+    }
+  });
+
 });
